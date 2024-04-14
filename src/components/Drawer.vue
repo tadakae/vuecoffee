bad7d4615af73496e010e13d445dc249a954541b
heisenberg <ramazan.sapar003@gmail.com>
2024-04-14 22:39:08 +0600

    commit

--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -1,15 +1,15 @@
 <template>
  <div class=" fixed top-0 left-0 h-full w-full bg-black z-10 opacity-50"></div>
   <div class=" bg-white w-96 h-full fixed right-0 top-0 z-20 p-8">
-      <h2 class=" text-2xl font-bold">Корзина</h2>
+      <DrawerHead />
+      <CartItem />
   </div>
 </template>
 
 <script setup>
+import DrawerHead from "./DrawerHead.vue";
+import CartItem from "./CartItem.vue";
 
 
 </script>
 
-<style scoped>
-
-</style>
---
+++ src/components/DrawerHead.vue
@@ -0,0 +1,8 @@
+<template>
+    <h2 class=" text-2xl font-bold">Корзина</h2>
+</template>
+
+<script setup>
+
+</script>
+

b944f996397e505c85f034024cbc8f4613a3af6d
heisenberg <ramazan.sapar003@gmail.com>
2024-05-07 20:29:49 +0600

    func

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,19 +1,34 @@
 <template>
     <div class="box  ml-10 mt-10 p-4 w-32 cursor-pointer rounded-md bg-orange-50 hover:bg-orange-100 active:bg-orange-200  ">
-        <img src="./assets/cappucino.jpg" alt="" class="img w-24 h-24 rounded-xl">
-        <div class=" text-sm/[17px] mt-1   font-extralight text-center">
-            <b>
-                Каппучино
+        <img  :src="imageUrl" alt="" class="img w-24 h-24 rounded-xl">
+
+
+        <div class=" text-sm/[17px] mt-1  font-extralight text-center block">
+            <b class="mt-2 p-2">
+                {{Title}}
             </b>
-            <b>
-                Цена: 790$
+            <span>
+                Цена:
+            <b class="text-slate-600 ">
+                {{Price}}
             </b>
+                </span>
         </div>
     </div>
 
 </template>
 
 <script setup>
+defineProps({
+    id: Number,
+    Title: String,
+    imageUrl: String,
+    Price: Number,
+    isFavorite: Boolean,
+    isAdded: Boolean,
+    onClickFavorite: Function,
+    onClickAdd: Function
+})
 
 </script>
 
---
+++ src/components/CardList.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+import Card from "@/components/Card.vue";
+
+defineProps({
+    items: Array
+})
+</script>
+
+
+<template>
+  <div class="grid grid-cols-6 ">
+      <Card
+              v-for="item in items"
+              v-bind:key="item.id"
+              :Title="item.Title"
+              :imageUrl="item.imageUrl"
+              :Price="item.Price"
+      />
+
+  </div>
+
+</template>

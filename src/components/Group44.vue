c7312da6ac09945754dc3fed2f8dcb5241ab3a52
roma <ramazan.sapar003@gmail.com>
2024-06-26 08:39:55 +0300

    26j

---
+++ src/components/Group44.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="w-full  bg-orange-100 ">
+  <img class="m-auto p-10" src="./assets/Group%2044.png" alt="">
+  </div>
+</template>
+
+<style scoped>
+
+</style>
